d7f1bc3d4645a36d2428634c77fe3d70864a14aa
Omotayo Michael <Omotayomichael390@gmail.com>
2023-09-07 22:50:16 -0700

    commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import './App.css'
+import './App.css';
+import { BrowserRouter as Router,Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
-    <>
-        
-    </>
+    <div className='App'>
+        <Router>
+          <Routes></Routes>
+        </Router>
+    </div>
   )
 }
 
---
+++ src/config/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAf-ML2D9HHImTo1ta4Rysm8ZQDAkJ-OdQ",
+  authDomain: "expense-tracker-f8eaf.firebaseapp.com",
+  projectId: "expense-tracker-f8eaf",
+  storageBucket: "expense-tracker-f8eaf.appspot.com",
+  messagingSenderId: "185529794760",
+  appId: "1:185529794760:web:7da25bd44cf44bb7d5e5fd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
